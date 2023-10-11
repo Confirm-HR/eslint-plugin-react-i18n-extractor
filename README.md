8b8f228c28c3ac06043a2a64cf02d0e7234ed711
Samuel Vrech <samuel.vrech@confirm.com>
2023-10-11 14:02:02 +0100

    initial copy of project

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# eslint-plugin-react-i18n-extractor
+# eslint-plugin-react-intl
+
+Helper for react-intl i18n
+
+## Installation
+
+You'll first need to install [ESLint](https://eslint.org/):
+
+```sh
+npm i eslint --save-dev
+```
+
+Next, install `eslint-plugin-react-intl`:
+
+```sh
+npm install eslint-plugin-react-intl --save-dev
+```
+or add it locally on a subdirectory after the checkout: 
+```
+"devDependencies": {
+    "@babel/eslint-parser": "^7.21.3",
+    "@typescript-eslint/eslint-plugin": "^5.43.0",
+    ...
+    "eslint-plugin-react-intl": "file:../eslint-plugin-react-intl",
+    ...
+}
+```
+
+## Usage
+
+Add `react-intl` to the plugins section of your `.eslintrc` configuration file. You can omit the `eslint-plugin-` prefix:
+
+```json
+{
+    "plugins": [
+        "react-intl"
+    ]
+}
+```
+
+
+Then configure the rules you want to use under the rules section.
+
+```json
+{
+    "rules": {
+        "react-intl/no-static-text": "warn"
+    }
+}
+```
+
+## Rules
+
+<!-- begin auto-generated rules list -->
+TODO: Run eslint-doc-generator to generate the rules list.
+<!-- end auto-generated rules list -->
+
+
---
+++ tests/lib/rules/no-static-text.js
@@ -0,0 +1,94 @@
+/**
+ * @fileoverview extract static text
+ * @author
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+// const parsers = require('../../helpers/parsers');
+
+const parserOptions = {
+  ecmaVersion: 2018,
+  sourceType: "module",
+  ecmaFeatures: {
+    jsx: true,
+  },
+};
+
+const rule = require("../../../lib/rules/no-static-text"),
+  RuleTester = require("eslint").RuleTester;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({ parserOptions });
+ruleTester.run("no-static-text", rule, {
+  valid: [
+    // give me some code that won't trigger a warning
+  ],
+
+  invalid: [
+    {
+      filename:
+        "somepath/src/components/another-sub/libraryAcc/foo/BarGammaZZs.ts",
+      code: `
+        class Comp1 extends Component {
+          render() {
+            return (<div>test</div>);
+          }
+        }
+      `,
+      errors: [
+        {
+          messageId: "literalNotInJSXExpression",
+          data: { text: "test" },
+        },
+      ],
+      output: `
+        class Comp1 extends Component {
+          render() {
+            return (<div><FormattedMessage id="app.components.another_sub.library_acc.foo.bar_gamma_z_zs." defaultMessage="test"/></div>);
+          }
+        }
+      `,
+    },
+    {
+      options: [
+        {
+          noAttributeStrings: true,
+          noAttributeStringsInclude: ["title"],
+        },
+      ],
+      filename:
+        "somepath/src/components/another-sub/libraryAcc/foo/BarGammaZZs.ts",
+      code: `
+        class Comp2 extends Component {
+          render() {
+            return (<div anotherAttribute="Can be string" title="Cannot be string">test</div>);
+          }
+        }
+      `,
+      errors: [
+        {
+          messageId: "noStringsInAttributes",
+          data: { text: '"Cannot be string"' },
+        },
+        {
+          messageId: "literalNotInJSXExpression",
+          data: { text: "test" },
+        },
+      ],
+      output: `
+        class Comp2 extends Component {
+          render() {
+            return (<div anotherAttribute="Can be string" title={formatMessage({id:"app.components.another_sub.library_acc.foo.bar_gamma_z_zs.title.cannot_be_string", defaultMessage:"Cannot be string"})}><FormattedMessage id="app.components.another_sub.library_acc.foo.bar_gamma_z_zs." defaultMessage="test"/></div>);
+          }
+        }
+      `,
+    },
+  ],
+});
